8c527cb1f9733f2fbbc52b9b6da886cef0f6dec9
Maxime SAINJON <maxsainjon@hotmail.fr>
2017-01-12 14:50:56 +0100

    first commit

---
+++ routes/songs.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const SongService = require('../services/SongService');
+
+router.post('/', (req, res) => {
+  return SongService.create(req.body)
+    .then(song => {
+      res.status(201).send(song);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    })
+  ;
+});
+
+router.get('/', (req, res) => {
+  SongService.find(req.query)
+    .then(songs => {
+      res.status(200).send(songs);
+    })
+  ;
+});
+
+router.get('/:id', (req, res) => {
+  SongService.findById(req.params.id)
+    .then(songs => {
+      res.status(200).send(songs);
+    })
+    .catch(err => { res.status(500).send(error)})
+  ;
+});
+
+router.delete('/delete/:id', (req, res) => {
+  SongService.delete(req.params.id)
+    .then(song => {
+      res.status(204).send();
+    })
+  ;
+});
+
+module.exports = router;
---
+++ services/SongService.js
@@ -0,0 +1,28 @@
+'use strict'
+const db = require('../database');
+
+exports.find = (query = {}) => {
+   return db.Songs.findAll({
+     where: query
+   });
+};
+
+exports.findById = id => {
+   return db.Songs.findById(id);
+};
+
+exports.create = (song) => {
+  const model = db.Songs.build(song);
+  return model.validate()
+    .then(err => {
+      if (err) {
+        return Promise.reject(err);
+      }
+      return model.save();
+    })
+  ;
+};
+
+exports.delete = id => {
+   return db.Songs.destroy({where: {id:id}});
+};

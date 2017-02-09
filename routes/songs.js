8d329e3b123e0604145e567e132256999a6854f2
Maxime SAINJON <maxsainjon@hotmail.fr>
2017-02-09 10:57:59 +0100

    maj

--- routes/songs.js
+++ routes/songs.js
@@ -20,13 +20,30 @@ router.get('/', (req, res) => {
   ;
 });
 
-router.get('/:id', (req, res) => {
+/*router.get('/:id', (req, res) => {
   SongService.findById(req.params.id)
     .then(songs => {
       res.status(200).send(songs);
     })
     .catch(err => { res.status(500).send(error)})
   ;
+});*/
+
+router.get('/:id', (req, res) => {
+  if (!req.accepts('text/html') && !req.accepts('application/json')) {
+       return res.status(406).send({err: 'Not valid type for asked resource'});
+   }
+   SongService.findOneByQuery({id: req.params.id}).then(song => {
+           if (!song) {
+               return res.status(404).send({err: `id ${req.params.id} not found`});
+           }
+           if (req.accepts('text/html')) {
+               return res.render('song', {song: song});
+           }
+           if (req.accepts('application/json')) {
+               return res.status(200).send(song);
+           }
+  });
 });
 
 router.delete('/delete/:id', (req, res) => {
